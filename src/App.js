ce9d721ade180307c0ba86e62e5cc6eae67c198e
OscarrrrW <zwang39@inside.artcenter.edu>
2026-04-15 09:57:39 -0400

    test1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TestComponent from "./components/TestComponent";
 
@@ -9,23 +9,7 @@ const Comp = () => {
 function App() {
   return (
     <div className="App" id='root'>
-            <TestComponent />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestComponent />
     </div>
   );
 }
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
 const TestComponent = () => {
-  return <p><b>Another test component</b></p>
-}
+  const [position, setPosition] = useState({ top: '40%', left: '40%' });
+
+  const moveButton = () => {
+    const newTop = `${Math.floor(Math.random() * 85)}%`;
+    const newLeft = `${Math.floor(Math.random() * 85)}%`;
+    setPosition({ top: newTop, left: newLeft });
+  };
+
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '400px' }}>
+      <button
+        onClick={moveButton}
+        style={{ position: 'absolute', top: position.top, left: position.left }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+};
 
 export default TestComponent;
