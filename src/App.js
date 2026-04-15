9c89ca197c72a7b85ff9b241551938f44b388e79
OscarrrrW <zwang39@inside.artcenter.edu>
2026-04-15 10:01:47 -0400

    test 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@
 import './App.css';
 import TestComponent from "./components/TestComponent";
 
-const Comp = () => {
-  return <p>This is a comp component</p>
-}
-
 function App() {
   return (
     <div className="App" id='root'>
